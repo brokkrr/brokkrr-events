b1a52adff8fd79dda6577aae18281f3ae03ca151
Dhruv Dang <hi@dhruv.io>
2019-01-08 21:10:30 -0800

    update purge script

--- purge.sh
+++ purge.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
 
-curl 'https://purge.jsdelivr.net/gh/brokkrr/brokkrr-events/build.js'
+#curl 'https://purge.jsdelivr.net/gh/brokkrr/brokkrr-events/build.js'
+
+curl -X POST \
+  http://purge.jsdelivr.net/ \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d '{
+	"path": [
+		"/gh/brokkrr/brokkrr-events/build.js"
+	]
+}'
